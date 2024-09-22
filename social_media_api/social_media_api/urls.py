de0b558974803de4b7d24268e45932278e049a9c
soufiane belhous <163477528+spoofy001@users.noreply.github.com>
2024-09-22 15:21:42 +0100

    Delete social_media_api directory

--- social_media_api/social_media_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('posts/', include('posts.urls')),
-    path('api/', include('api.urls')),
-]
